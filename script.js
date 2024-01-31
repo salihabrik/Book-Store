74a18ad95d9f0a0be3272bfaee15cd93292ae5bd
salihabrik <briksaliha@gmail.com>
2024-01-31 19:35:49 +0100

    update

--- script.js
+++ script.js
@@ -1,33 +1,49 @@
-searchForm = document.querySelector('.search-form');
+let searchForm = document.querySelector('.search-form');
+let navbar = document.querySelector('.navbar'); // Add this line if you have a navbar element
 
-document.querySelector('#search-btn').onclick = () =>{
+document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
     navbar.classList.remove('active');
-    
 }
 
 let loginForm = document.querySelector('.login-form-container');
 
-document.querySelector('#login-btn').onclick = () =>{
+document.querySelector('#login-btn').onclick = () => {
     loginForm.classList.toggle('active');
-    
 }
-document.querySelector('#close-login-btn').onclick = () =>{
+
+document.querySelector('#close-login-btn').onclick = () => {
     loginForm.classList.remove('active');
 }
 
 window.onscroll = () => {
-    if(window.scrollY > 80) {
+    if (window.scrollY > 80) {
         document.querySelector('.header .header-2').classList.add('active');
-    }else {
+    } else {
         document.querySelector('.header .header-2').classList.remove('active');
-    }   
+    }
 }
 
 window.onload = () => {
-    if(window.scrollY > 80) {
+    if (window.scrollY > 80) {
         document.querySelector('.header .header-2').classList.add('active');
-    }else {
+    } else {
         document.querySelector('.header .header-2').classList.remove('active');
-    }   
+    }
 }
+
+var swiper = new Swiper(".books-slider", {
+    loop: true,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+            768: {
+                slidesPerView: 2,
+            },
+            1024: {
+                slidesPerView: 3,
+            },
+        },
+    
+});
