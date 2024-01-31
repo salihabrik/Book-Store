82d13614ce08f2f96c0aafdee89dd4a69ba47265
salihabrik <briksaliha@gmail.com>
2024-01-31 13:32:48 +0100

    update

---
+++ script.js
@@ -0,0 +1,23 @@
+searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    
+}
+
+window.onscroll = () => {
+    if(window.scrollY > 80) {
+        document.querySelector('.header .header-2').classList.add('active');
+    }else {
+        document.querySelector('.header .header-2').classList.remove('active');
+    }   
+}
+
+window.onload = () => {
+    if(window.scrollY > 80) {
+        document.querySelector('.header .header-2').classList.add('active');
+    }else {
+        document.querySelector('.header .header-2').classList.remove('active');
+    }   
+}
