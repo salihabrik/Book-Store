d8841bb737ce2b770bcdc7f9968f2efcef1786b5
salihabrik <briksaliha@gmail.com>
2024-01-31 13:55:39 +0100

    update

--- script.js
+++ script.js
@@ -6,6 +6,16 @@ document.querySelector('#search-btn').onclick = () =>{
     
 }
 
+let loginForm = document.querySelector('.login-form-container');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+    
+}
+document.querySelector('#close-login-btn').onclick = () =>{
+    loginForm.classList.remove('active');
+}
+
 window.onscroll = () => {
     if(window.scrollY > 80) {
         document.querySelector('.header .header-2').classList.add('active');
